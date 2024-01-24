5d6d36277f797ad4c0a4b5d012a28770ce8603cf
bllor <duki0105@gmail.com>
2024-01-25 08:57:56 +0900

    python day1

---
+++ 3.파이썬의 간략한 소개.py
@@ -0,0 +1,16 @@
+# 24.1.25
+# 파이썬 공식 문서 튜토리얼
+
+#ex1
+print(2+4)
+
+#ex2
+# 5**2를 하게 되면 제곱이 된다.
+print(5**2)
+
+#ex3
+tax = 12.5 / 100
+price = 100.50
+totalPrice = tax*price
+print(totalPrice)
+print(round(totalPrice,2))
