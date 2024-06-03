33c3249d81a0682955b2f7ee2656feb2190cff9c
bllor <duki0105@gmail.com>
2024-06-04 07:13:43 +0900

    6063

--- codeup/test.py
+++ codeup/test.py
@@ -3,5 +3,11 @@ a,b = c.split(' ')
 print(int(a) - int(b))
 
 d = ['+','-','*','//']
+'''
+for i in len(d):
+    print(1+int(d[i])+2)
+이게 안되었던 이유는 len(d)으로 숫자만 출력했고 범위를 나타내지 않았기 때문
+'''
+
 for i in range(len(d)):
     print(1+int(d[i])+2)
