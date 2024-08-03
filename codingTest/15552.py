9497c5ae18aa33d22fa6c6ad39d64e8c7f0410cf
bllor <duki0105@gmail.com>
2024-08-03 15:04:28 +0900

    24.8.3

--- codingTest/15552.py
+++ codingTest/15552.py
@@ -2,5 +2,7 @@ import sys
 
 n = int(input())
 for i in range(n):
-    a,b = sys.stdin.readline().rstrip()
+    a,b = sys.stdin.readline().split(' ')
+    a= int(a.rstrip())    
+    b= int(b.rstrip())
     print(a+b)
