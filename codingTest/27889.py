2fdc494acf37af78b95f0841ec89f2ab7008e4b8
bllor <duki0105@gmail.com>
2024-08-08 22:12:06 +0900

    27889

---
+++ codingTest/27889.py
@@ -0,0 +1,10 @@
+sn = input()
+
+if sn == 'NLCS':
+    print('North London Collegiate School')
+elif sn == 'BHA':
+    print('Branksome Hall Asia')
+elif sn == 'KIS':
+    print('Korea International School')
+else:
+    print('St. Johnsbury Academy')
